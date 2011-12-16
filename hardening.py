6b081b1540023f713c57e4c8c91157a7ce71906e
Frédéric Masi <frederic.masi2@gmail.com>
2011-12-16 15:11:13 +0000

    Added note regarding the need to run the script as rood for folders that do not have the x bit setup. Added function to setup auditd on at boot time.

--- hardening.py
+++ hardening.py
@@ -8,6 +8,7 @@ import sys
 def recursive_chmod(dir_name, permition):
 """
 Recursive chmod.
+This function takes two arguments the first one is the name of the folder and the second is the permition you like to recursively set.
 ###FIX-ME### I ONLY WORK WITH FOLDERS. I DONT WORK IF YOU PASS ME A FILENAME
 """
 	for root, dirs, files in os.walk(dir_name):
@@ -19,6 +20,8 @@ Recursive chmod.
 
 if __name__='__main__':
 	# Seting up permitions to comply with GEN003080 specifications
+	# Attention if you don't set the x bit on folders and you need to run this script a second time
+	# You will need to run it as root
 	try:
 		recursive_chmod('/var/spool/cron', 0600)
 		recursive_chmod('/etc/cron.d', 0600)
@@ -29,3 +32,12 @@ if __name__='__main__':
 		recursive_chmod('/etc/cron.weekly', 0700)
 	except OSError as error:
 		print >> sys.stderr, 'You got an OS error:\n', str(error)
+	
+	# Seting up auditd to comply with GEN002720 specifications
+	chkconfig_exit_status = os.system('chkconfig auditd on')
+	if chkconfig_ext_status == 0:
+		print >> sys.stdout, "Audit service successfully setup to start at boot time."
+	else:
+		print >> sys.stderr, "There was a problem when seting up Audit service at boot time. Exit with error code: ", chkconfig_ext_status
+		exit('error with chkconfig auditd on')
+	
