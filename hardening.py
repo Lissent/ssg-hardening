3f658106f23f42b9fd9efc4bf7327e9e1f601748
Frédéric Masi <frederic.masi2@gmail.com>
2012-01-12 17:15:10 +0100

    Menu setup
    Added a command line menu interface.
    Still need to make the appropriate calls to the required modules upon
    fag creating but the menu is created.

--- hardening.py
+++ hardening.py
@@ -22,6 +22,8 @@ This scripts implements changes required to meet the specifications descrideb by
 import os
 import sys
 import grp
+from optparse import OptionParser
+from optparse import OptionGroup
 
 
 def recursive_chmod(dir_name, permition):
@@ -38,7 +40,7 @@ def recursive_chmod(dir_name, permition):
 			os.chmod(os.path.join(root, file), permition)
 
 
-def gen003080():
+def gen3080():
 	'''
 	Seting up permitions to comply with GEN003080 specifications
 	# Attention if you don't set the x bit on folders and you need to run this script a second time
@@ -56,7 +58,7 @@ def gen003080():
 		print >> sys.stderr, 'You got an OS error:\n', str(error)
 
 
-def gen000580():
+def gen580():
 	'''
 	This function sets up pam.d and falgs all passwords as expired so that the system will comply with: "GEN000580 PASSWORD LENGTH"
 	###FIX ME###
@@ -69,7 +71,7 @@ def gen000580():
 		print >> sys.stderr, "There was a problemm setuing up the password configuration files:\n", str(error)
 
 
-def gen002720_gen002740_gen002760():
+def gen2720_gen2740_gen2760():
 	'''
 	This fuction configurs auditd to comply with GEN002720 GEN002740 and GEN002760 specifications
 	'''
@@ -93,7 +95,7 @@ def gen002720_gen002740_gen002760():
 		print >> sys.stderr, 'There was a problem seting up the Audit rules file:\n', str(error)
 
 
-def gen003200():
+def gen3200():
 	'''
 	Seting up permitions of /etc/cron.deny to comply with GEN003200
 	'''
@@ -104,7 +106,7 @@ def gen003200():
 		print >> sys.stderr, "There was a problem seting up premitions for /etc/cron.deny error:\n", str(error)
 
 
-def gen005320_gen005360():
+def gen5320_gen5360():
 	'''
 	Seting up permitions of /etc/snmp/snmpd.conf to comply with GEN005320.
 	Seting up ownership of snmpd.conf and .mib to comply with GEN005360 root and sys
@@ -127,7 +129,7 @@ def gen005320_gen005360():
 	'''
 
 
-def gen005400():
+def gen5400():
 	'''
 	Seting up ownership and permitions of /etc/rsyslog.conf to comply with GEN005400.
 	###FIX ME###
@@ -139,7 +141,8 @@ def gen005400():
 	except OSError as error:
 		print >> sys.stderr, "There was a problem setting up the ownership and/or permitions of /etc/rsyslog.conf:\n", str(error)
 
-def lnx00440():
+
+def lnx440():
 	# Seting up file permitions for /etc/security/access.conf to comply with LNX00440
 	"Note that by default on a ssg 6.1 the permition of this file is 640"
 	try:
@@ -148,8 +151,53 @@ def lnx00440():
 		print >> sys.stderr, "There was a problem setting up the permitions of /etc/security/access.conf:\n", str(error)
 
 
+def menu_setup():
+	# setup usage message
+	usage = "usage: %prog [options]"
+	# setup the parser and the defaul values
+	parser = OptionParser()
+	parser.set_defaults(all=False)
+	parser.set_defaults(gen3080=False)
+	parser.set_defaults(gen580=False)
+	parser.set_defaults(audit=False)
+	parser.set_defaults(gen3200=False)
+	parser.set_defaults(snmp=False)
+	parser.set_defaults(gen5400=False)
+	parser.set_defaults(lnx440=False)
+	
+	# set the desire main options
+	parser.add_option("-f", "--file", dest="filename",
+										help="Use FILE as destination istead of the system syslog", metavar="FILE")
+	parser.add_option("-a", "--all",
+										action='store_true', dest='all',
+										help='Turn on all the compliance modules')
+	# Set up a menu group and its options
+	group = OptionGroup(parser, "Compliance modules options",
+	                    "This options enable specific modules on demand. The --all option will enable all of this options")
+	# Creat the options for the Complinace module group
+	group.add_option("--gen3080", action="store_true", dest='gen3080',
+									help="Fixes file permitions to comply with GEN3080")
+	group.add_option('--gen580', action='store_true', dest='gen580',
+									help='Set-up pam.d to comply with GEN580')
+	group.add_option('--audit', action='store_true', dest='audit',
+									help='Set-up audit.d to comply with gen2720, gen2740 and gen2760')
+	group.add_option('--gen3200', action='store_true', dest='gen3200',
+									help='Set-up permition for cron.deny to comply with GEN3200')
+	group.add_option('--snmp', action='store_true', dest='snmp',
+									help='Set-up permition and ownership of the snmp files to comply with GEN5320 and GEN5360')
+	group.add_option('--gen5400', action='store_true', dest='gen5400',
+									help='Set-up permitions and ownership of the loging system to comply with GEN4500')
+	group.add_option('--lnx440', action='store_true', dest='lnx440',
+									help='Set-up ownership of access.conf to comply with LNX440')
+	
+	parser.add_option_group(group)
+	
+	# Parse the comand line
+	(options, args) = parser.parse_args()
+
+
 def main():
-	pass
+	menu_setup()
 
 
 if __name__ == '__main__':
