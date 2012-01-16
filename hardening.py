Lissent/ssg-hardening

commit f0065dc74f77e4a73cd3b2574bc09ff16f883457

Default acction
Enable the -all flag by default.

hardening.py
@@ -156,7 +156,7 @@ def menu_setup():
 	usage = "usage: %prog [options]"
 	# setup the parser and the defaul values
 	parser = OptionParser()
-	parser.set_defaults(all=False)
+	parser.set_defaults(all=True)
 	parser.set_defaults(gen3080=False)
 	parser.set_defaults(gen580=False)
 	parser.set_defaults(audit=False)
@@ -171,7 +171,7 @@ def menu_setup():
 										
 	parser.add_option("-a", "--all",
 										action='store_true', dest='all',
-										help='Turn on all the compliance modules')
+										help='Turn on all the compliance modules - This is the DEFAULT')
 										
 	# Set up a menu group and its options
 	group = OptionGroup(parser, "Compliance modules options",

commit 8075723a86ccaea60226dd08cd28a19d9cbe2f3f

Quick fix
This is a quick and dirty fix for the problem introduced by commit
f0065dc74f77e4a73cd3b2574bc09ff16f883457

Need to find a better way to browse true the options or a way to mark
options.all to False if any other option is passed.

hardening.py
@@ -207,6 +207,13 @@ def menu_setup():
 
 def main():
 	(options, agrs) = menu_setup()
+	
+	"""Need a better way to check for module otpinos"""
+	option_list = [options.gen3080, options.gen580, options.audit, options.gen3200, options.snmp, options.gen5400, options.lnx440]
+	for item in option_list:
+		if item == True:
+			options.all = False
+			
 	# Checks if lunching all options is desired. If so sets all of them to true
 	if options.all == True:
 		options.gen3080 = True